9f0f1789b7b1275326a7800ea632bf03ab31774e
Quinntyne Brown <quinntynebrown@gmail.com>
2015-12-10 22:22:17 -0500

    clean up and done

--- wwwroot/app/components/feed-list.ts
+++ wwwroot/app/components/feed-list.ts
@@ -1,35 +1,19 @@
 ﻿class FeedListComponent {
-    constructor(private $location, $routeParams, private dispatcher, feedActions, feedStore) {        
-        this.feedItems = feedStore.items.filter(item => {
-            if ($routeParams.username) {
-                return item.owner.login.toLowerCase() === $routeParams.username;
+    constructor(private $location, private $routeParams, private feedStore) { }
+
+    username:string;
+
+    get feedItems() {
+        return this.feedStore.items.filter(item => {
+            if (this.$routeParams.username) {
+                return item.owner.login.toLowerCase() === this.$routeParams.username;
             }
             return item.owner.login.toLowerCase() === "quinntynebrown";
         });
-        this.listenerId = dispatcher.addListener({
-            actionType: "CHANGE",
-            callback: options => {
-                this.feedItems = feedStore.items.filter(item => {
-                    return item.owner.login.toLowerCase() === $routeParams || "quinntynebrown";
-                });
-            }
-        });
     }
-
-    username;
-
-    feedItems;
     
-    listenerId;
-
-    query() {
-        this.$location.path("/" + this.username);
-    }
-
-    deactivate() {
-        this.dispatcher.removeListener({ id: this.listenerId });
-    }
-
+    query() { this.$location.path("/" + this.username); }
+    
     static canActivate = () => {        
         return ["$q", "$route", "dispatcher", "feedActions", ($q, $route, dispatcher, feedActions) => {
             var deferred = $q.defer();            
@@ -51,7 +35,7 @@
 ngX.Component({
     component: FeedListComponent,
     styles: [" .feedList {}"].join(),
-    providers: ["$location","$routeParams","dispatcher","feedActions","feedStore"],
+    providers: ["$location","$routeParams","feedStore"],
     template: [
         "<div>",
         "<h1>Feed List</h1>",
